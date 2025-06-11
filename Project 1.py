78665bc6eea44f55849a92e74fb47c5cfad991e0
Laney <153005822+Fuxcian@users.noreply.github.com>
2025-06-11 05:03:23 -0500

    File Starter
    File sent just to start repository and allow team members to work on it as well.

---
+++ Project 1.py
@@ -0,0 +1,15 @@
+#Imports
+import json
+import ssl
+from urllib.request import urlopen, Request
+
+def main():
+    #Read live data from a file
+    pass
+
+
+#NOTE: Client wishes to study behavior of wikipedia editors.
+    #Must be able to type Wiki article title in command line
+    #Must display recent edits to that article: oldest to newest, display all if less than 30
+        #time of change, name of editor, new line
+    #Exit with error code 0
